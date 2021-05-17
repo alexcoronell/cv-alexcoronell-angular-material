bd94147e63c9a73aecd7bbbdf88895bc06582cf8
Alex Coronell <alexcoronell@gmail.com>
2021-05-17 11:45:21 -0500

    Maquetación estudios y carga de información json

--- src/app/components/studies/studies.component.ts
+++ src/app/components/studies/studies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data from  '../../../assets/data/studies.json';
 
 @Component({
   selector: 'app-studies',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudiesComponent implements OnInit {
 
+  titlePage:string = "Estudios";
+  studiesData:any = data;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.studiesData);
   }
 
 }
